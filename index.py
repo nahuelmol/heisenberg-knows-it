be7645de3f26532603119d945c6f3b09038f0886
unknown <kindermolina98@gmail.com>
2025-09-02 12:03:49 -0300

    first commit;

---
+++ index.py
@@ -0,0 +1,57 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+BOT_TOKEN = os.getenv("TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
+if not BOT_TOKEN or not WEBHOOK_URL:
+        print("Error: BOT_TOKEN o WEBHOOK_URL not setted")
+        return
+
+class Command:
+    def __init__(self, cmd):
+        self.cmd = cmd
+        self.root = cmd[0][1:]
+        self.options = None
+        self.message = ''
+
+    def set(self):
+        if self.root == 'cal':
+            if len(self.cmd) > 1:
+                self.action = self.cmd[1]
+                if len(self.cmd) > 2:
+                    self.options = self.cmd[2:]
+            else:
+                self.message = 'few args'
+
+    def execute(self):
+        if self.root == 'cal':
+            if self.action == 'req':
+                self.message = 'making a request'
+            else:
+                self.message = 'not recognized action'
+        else:
+            self.message = 'not recognized cmd'
+
+async def switch(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    string_cmd = update.message.text.split(" ")
+    cmd = Command(string_cmd)
+    cmd.set()
+    cmd.execute()
+
+    await update.message.reply_text(f'{cmd.message}')
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler('cal', switch))
+#app.run_polling()
+
+app.run_webhook(
+    listen="8.0.0.0",
+    port=int(os.getenv("PORT", "5000")),
+    url_path=BOT_TOKEN,
+    webhook_url=f"{WEBHOOK_URL}/{BOT_TOKEN}",
+)
+
+if __name__ == "__main__":
+    main()
+
